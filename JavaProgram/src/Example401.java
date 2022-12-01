6fb161fc4a13250d593956e1df198762edc5efee
Panachaifame <panachai.fame@gmail.com>
2022-12-01 16:08:16 +0700

    Exercise 12-1-2022

---
+++ JavaProgram/src/Example401.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Example401 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String inputword="";
+		String txtWord = "";
+		while(true) {
+		System.out.print("Enter word : ");
+		inputword = scan.next();
+		if(inputword.equalsIgnoreCase("stop")){
+			break;
+			}
+		txtWord = txtWord+inputword+" ";
+		}
+			System.out.print(txtWord.toUpperCase());
+	}
+}
